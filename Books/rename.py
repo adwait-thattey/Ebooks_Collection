db005987cb34f5885fec159446e9a55e9c1f9f09
unknown <bajiraoayush@gmail.com>
2019-06-23 22:15:17 +0530

    More books added.

--- Books/rename.py
+++ Books/rename.py
@@ -1,6 +1,9 @@
 import os
 for filename in os.listdir("."):
-    if filename.find(' - ') != -1:
-        index_change = filename.find(' - ')
-        new_name = filename[index_change+2:-5] + ' - ' + filename[:index_change] + '.epub'
-        os.rename(filename, new_name)
+    if filename.find('.') != -1:
+        
+        new_name = filename.title()
+        new_name = new_name[:-4] + 'epub'
+        new_name = new_name.replace('(Bookos-Z1.Org)', '')
+        print(filename,new_name)
+        # os.rename(filename, new_name)
